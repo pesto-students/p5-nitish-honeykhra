5165f77e476526ea86aa2ef9bc07e4e1789d9428
honeykhra <honeykhra@gmail.com>
2022-09-07 21:47:59 +0530

    basic file system created for assignments

---
+++ Week-4/Assignment2/script.js
@@ -0,0 +1,30 @@
+(function (logger) {
+  console.old = console.log;
+  console.log = function () {
+    var output = "",
+      arg,
+      i;
+
+    for (i = 0; i < arguments.length; i++) {
+      arg = arguments[i];
+      output += '<span class="log-' + typeof arg + '">';
+
+      if (
+        typeof arg === "object" &&
+        typeof JSON === "object" &&
+        typeof JSON.stringify === "function"
+      ) {
+        output += JSON.stringify(arg);
+      } else {
+        output += arg;
+      }
+
+      output += "</span>&nbsp;";
+    }
+
+    logger.innerHTML += output + "<br>";
+    console.old.apply(undefined, arguments);
+  };
+})(document.getElementById("logger"));
+
+// ! Code starts here
