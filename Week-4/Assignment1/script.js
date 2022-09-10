d1e6e64aadeac32e0fb212dcb381c0f3ad6b0e33
honeykhra <honeykhra@gmail.com>
2022-09-10 15:42:16 +0530

    changed names

--- Week-4/Assignment1/script.js
+++ Week-4/Assignment1/script.js
@@ -27,47 +27,4 @@
   };
 })(document.getElementById("logger"));
 
-var log = console.log;
-
 // ! Code starts here
-var Person = function () {};
-
-Person.prototype.initialize = function (name, age) {
-  this.name = name;
-  this.age = age;
-};
-
-// TODO: Create a class Teacher and a method teach
-class Teacher extends Person {
-  constructor(name, age) {
-    super(name, age); // call the super class constructor and pass in the name parameter
-  }
-  teach(sub) {
-    let subject;
-    subject = sub;
-    log(`${this.name} is now teaching ${subject}`);
-  }
-}
-
-Teacher.prototype.tution = function (subject) {
-  let tutionSubject = subject;
-  log(`${this.name} is now teaching ${tutionSubject} at home`);
-};
-
-const him = new Teacher();
-him.initialize("Adam", 45);
-log(him);
-him.teach("inheritance");
-him.tution("ReactJs");
-
-// ? ES6 method
-// class Person {
-//   initialize(name, age) {
-//     this.name = name;
-//     this.age = age;
-//   }
-//   return() {
-//     console.log(`Person's name is ${this.name}`);
-//     console.log(`Person's age is ${this.age}`);
-//   }
-// }
