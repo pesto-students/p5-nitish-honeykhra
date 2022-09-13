bab92dccc623677e7f89d94ae2239a6f457926a2
honeykhra <honeykhra@gmail.com>
2022-09-13 11:35:04 +0530

    Added overview of a promise

---
+++ Week-4/Assignment1/script.js
@@ -0,0 +1,18 @@
+// ? Overview of a promise
+// const promise = new Promise((resolve, reject) => {
+/*
+    Code logic goes here and you call  resolve(value)
+    or reject(error) to resolve or reject the promise
+ */
+// });
+
+// promise
+//   .then((value) => {
+// Code logic on success of an operation
+//   })
+//   .catch((error) => {
+// Code logic on failure of an operation
+//   })
+//   .finally(() => {
+// Code logic to be executed after completion of operation
+//   });
