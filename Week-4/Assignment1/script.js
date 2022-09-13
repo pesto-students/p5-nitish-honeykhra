27af4f44cb8fd53afd4649c52842e5ccc4cd85f1
honeykhra <honeykhra@gmail.com>
2022-09-13 11:37:53 +0530

    added required functionality using es6 promises

--- Week-4/Assignment1/script.js
+++ Week-4/Assignment1/script.js
@@ -16,3 +16,36 @@
 //   .finally(() => {
 // Code logic to be executed after completion of operation
 //   });
+
+// ? ES6 implementation of Promise using javascript promise
+
+const getNumber = () =>
+  new Promise((res, rej) => {
+    // const randomNumber = 10;
+    const randomNumber = Math.floor(Math.random() * 10);
+    setTimeout(() => {
+      res(randomNumber);
+    }, randomNumber * 10);
+  });
+
+const clickHandler = () => {
+  display("Loading...");
+  const numberPromise = getNumber();
+  numberPromise
+    .then((value) => incrementBy(value, 10))
+    .then((value) => incrementBy(value, 20))
+    .then((value) => incrementBy(value, 30))
+    .then(display)
+    .catch(display);
+};
+
+const display = (content) => {
+  text.innerText = content;
+};
+
+const incrementBy = (original, incrementValue) => {
+  console.log(original + incrementValue);
+  return original + incrementValue;
+};
+
+button.addEventListener("click", clickHandler);
