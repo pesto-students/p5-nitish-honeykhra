ca1a300249cef4f761c796bb6aa8d0dd76295728
honeykhra <honeykhra@gmail.com>
2022-09-17 00:13:20 +0530

    assignment 3 completed

--- Week-5/Assignment1/script.js
+++ Week-5/Assignment1/script.js
@@ -1,1 +1,7 @@
+async function fetchUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  return users.map((user) => console.log(user.name));
+}
 
+console.log(fetchUsers());
