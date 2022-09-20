161f0a3e384c693108f925687699425863d6040f
honeykhra <honeykhra@gmail.com>
2022-09-20 20:50:11 +0530

    tests added for all math operations

---
+++ Week-5/Assignment4/calculator.test.js
@@ -0,0 +1,43 @@
+const mathOperations = require("./calculator");
+
+// describe("Calculator tests", () => {
+//   test("adding 1 + 2 should return 3", () => {
+//     expect(mathOperations.sum(1, 2)).toBe(3);
+//   });
+// });
+
+// describe("Calculator tests", () => {
+//   test("adding 1 + 2 should return 10", () => {
+//     // arrange and act
+//     var result = mathOperations.sum(1, 2);
+
+//     // assert
+//     expect(result).toBe(10);
+//   });
+// });
+
+describe("Calculator tests", () => {
+  test("adding 1 + 2 should return 3", () => {
+    // arrange and act
+    var result = mathOperations.sum(1, 2);
+
+    // assert
+    expect(result).toBe(3);
+  });
+
+  test("subtracting 2 from 10 should return 8", () => {
+    // arrange and act
+    var result = mathOperations.diff(10, 2);
+
+    // assert
+    expect(result).toBe(8);
+  });
+
+  test("multiplying 2 and 8 should return 16", () => {
+    // arrange and act
+    var result = mathOperations.product(2, 8);
+
+    // assert
+    expect(result).toBe(16);
+  });
+});
