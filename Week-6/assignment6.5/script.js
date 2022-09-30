05ca6eab3ad623dbc84a42ede17cb9e70a69c8b7
honeykhra <honeykhra@gmail.com>
2022-09-30 20:22:09 +0530

    files added for assignment6.6

--- Week-6/assignment6.5/script.js
+++ Week-6/assignment6.5/script.js
@@ -5,6 +5,7 @@ function pairWithDifference(array, difference) {
     // console.log(array[i]);
 
     let a = array[i];
+
     for (let j = 0; j < n; j++) {
       let b = array[j];
       if (a - b === difference) {
@@ -16,6 +17,8 @@ function pairWithDifference(array, difference) {
   }
 }
 
-const A = [5, 10, 3, 2, 50, 80];
+const A = [5, 10, 3, 2, 50, 800];
 const B = 78;
 console.log("Pair with difference : ", pairWithDifference(A, B));
+
+
---
+++ Week-6/assignment6.6/script.js
@@ -0,0 +1,13 @@
+// given array S = {-1 2 1 -4}, and target = 1.
+//  The sum that is closest to thetarget is 2.(-1 + 2 + 1 = 2)
+
+function findTriplet(arr, target) {
+  // sort array (a,b)=>a-b
+  // loop through array
+  // Two pointers
+  // First pointer on second element
+  // Second pointer will be on n-1
+  // while pionter 1 < pointer 2
+
+  [-4, -1, 1, 2];
+}
