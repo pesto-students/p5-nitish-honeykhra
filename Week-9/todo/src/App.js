084e49d319f1425608cc0d5b4e0486b34077ed08
honeykhra <honeykhra@gmail.com>
2022-11-09 21:18:55 +0530

    basic structure created for todo app

---
+++ Week-9/todo/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./components/Form";
+import Heading from "./components/Heading";
+
+const App = () => {
+  return (
+    <div className="card">
+      <Heading />
+      <Form />
+    </div>
+  );
+};
+
+export default App;
