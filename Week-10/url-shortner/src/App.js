806b26dfab0b1ef27b461c41618a2c820933e438
honeykhra <honeykhra@gmail.com>
2022-11-25 21:29:50 +0530

    statics boost footer component data added

--- Week-10/url-shortner/src/App.js
+++ Week-10/url-shortner/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Statistics from "./components/Statistics";
+import Boost from "./components/Boost";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Statistics />
+      <Boost />
+      <Footer />
     </div>
   );
 }
